8cddb83138d563a331c91fc9e146068d2ea181c0
SergeyMarkelov <avemarksun@gmail.com>
2021-10-12 20:01:32 +0200

    Zrobiłem

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,10 +11,43 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        int numerIndexu = 8;
+
+        for (int zmiennaDoPentli = 100; zmiennaDoPentli >= 0; zmiennaDoPentli-- )
+        { if (zmiennaDoPentli%numerIndexu ==0 ){
+            System.out.println(zmiennaDoPentli);}
+        }
+
+
+
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+
+
+        int[] tablicaDoCzwiczenia = new int[5];
+        int zmiennaWprowadzoneDane;
+
+        Scanner zmiennaWprowadzaniadanych = new Scanner(System.in);
+
+        for (int y = 0; y <= tablicaDoCzwiczenia.length - 1; y++)
+
+        {
+            System.out.println( "prosze podac liczbe do konteneru: ");
+            tablicaDoCzwiczenia [y] = zmiennaWprowadzaniadanych.nextInt()*11;
+
+            System.out.println( "Kontener " + y + " = " + tablicaDoCzwiczenia [y]);
+        }
+
+        System.out.println("......");
+        int z = 0;
+        while (z <= tablicaDoCzwiczenia.length-1) {
+            System.out.println(tablicaDoCzwiczenia[z]);
+            z++;
+        }
+
+
 
     }
 }
